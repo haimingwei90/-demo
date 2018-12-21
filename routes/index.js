818a61ad3330f5cc1df462f68303b099a915e37c
haiming90 <weihaiming90@163.com>
2018-12-21 23:50:40 +0800

    chat.css change

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,7 @@ function avl(name,password){
     if(name == user[name].name && password == user[name].password){
       return user[name];
     }
+    return null;
 };
 router.post("/registe",function(req,res,next){
   const{name,password} = req.body;
@@ -19,12 +20,10 @@ router.post("/registe",function(req,res,next){
   res.redirect('/');
 })
 router.post("/login",function(req,res){
-  const { name, password } = req.body;
+  const {name, password } = req.body;
   const user = avl(name,password);
   console.log(user);
   if (user) {
-    
-    
     return  res.render('chat', { user })
   }
   res.redirect('/');
