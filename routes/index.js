cecbd98ad374746fd2ef47bcad5957a61969b3b5
haiming90 <weihaiming90@163.com>
2018-12-21 20:58:30 +0800

    add chat.ejs css update index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var user={};
+router.get('/', function(req, res, next) {  
+  // const{name,password} = req.body;
+  // if (usr = avl(name,password)){
+  //   res.render('chat',{user})
+  // }
+  res.render('index');
 });
+// function avl(name,password){
+//     if(name == user.name && password == user.password){
+//       return user[name];
+//     }
+// };
+// router.post("/registe",function(req,res,next){
+//   const{name,password} = req.body;
+//   user[name] = {name,password};
+//   res.redirect('/');
+// })
 
 module.exports = router;
