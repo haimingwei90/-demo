761615d3efd419ebb2478c723e2747821abf437a
haiming90 <weihaiming90@163.com>
2018-12-21 22:25:26 +0800

    login registe ok

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,30 @@ var router = express.Router();
 /* GET home page. */
 var user={};
 router.get('/', function(req, res, next) {  
-  // const{name,password} = req.body;
-  // if (usr = avl(name,password)){
-  //   res.render('chat',{user})
-  // }
+  
   res.render('index');
 });
-// function avl(name,password){
-//     if(name == user.name && password == user.password){
-//       return user[name];
-//     }
-// };
-// router.post("/registe",function(req,res,next){
-//   const{name,password} = req.body;
-//   user[name] = {name,password};
-//   res.redirect('/');
-// })
-
+function avl(name,password){
+    if(name == user[name].name && password == user[name].password){
+      return user[name];
+    }
+};
+router.post("/registe",function(req,res,next){
+  const{name,password} = req.body;
+  user[name] = {name,password};
+  console.log(user[name]);
+  
+  res.redirect('/');
+})
+router.post("/login",function(req,res){
+  const { name, password } = req.body;
+  const user = avl(name,password);
+  console.log(user);
+  if (user) {
+    
+    
+    return  res.render('chat', { user })
+  }
+  res.redirect('/');
+})
 module.exports = router;
