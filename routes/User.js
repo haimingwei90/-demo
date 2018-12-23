0a40642ed0ae5f120118bc56ad44c3621f96568d
haiming90 <weihaiming90@163.com>
2018-12-23 17:44:11 +0800

    user.save ok

--- routes/User.js
+++ routes/User.js
@@ -6,12 +6,12 @@ const User = mongoose.model("User",(
     password:String
         }
 ));
-async function run(){
-    await User.remove();
-    await User.create(
-       {name:"hai",password:"hai"}
-    )
-    console.log(await User.find());    
-}
-run();
-// module.exports = User;
+// async function run(){
+//     await User.remove();
+//     await User.create(
+//        {name:"hai",password:"hai"}
+//     )
+//     console.log(await User.find());    
+// }
+
+ module.exports = User;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var User = require("./User.js");
 /* GET home page. */
-var user={};
 router.get('/', function(req, res, next) {  
   console.log(req.session.user);
   
@@ -10,10 +10,19 @@ router.get('/', function(req, res, next) {
   }
     res.render('index');
 });
-router.post("/registe",function(req,res,next){
+router.post("/registe",async function(req,res,next){
+  
   const{name,password} = req.body;
-  user[name] = {name,password};
-  console.log(user[name]);
+  var user = new User({name:name,password:password});
+  await user.save(function (err){
+    if(err){
+      console.log(err);
+    }else{
+      console.log("save ok");
+      
+    }
+  });
+  console.log(await User.find());
   
   res.redirect('/');
 })
