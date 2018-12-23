bdc1efa6be6e172ec6cb128d4d967c40ceb7bc1c
haiming90 <weihaiming90@163.com>
2018-12-23 17:07:45 +0800

    create User.js

---
+++ routes/User.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+mongoose.Promise = Promise;
+mongoose.connect("mongodb://localhost/test",{useMongoClient:true});
+const User = mongoose.model("User",(
+    {name:String,
+    password:String
+        }
+));
+async function run(){
+    await User.remove();
+    await User.create(
+       {name:"hai",password:"hai"}
+    )
+    console.log(await User.find());    
+}
+run();
+// module.exports = User;
