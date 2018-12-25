c604c869e1145e18729e8696863d5dbdaae37136
haiming90 <weihaiming90@163.com>
2018-12-26 00:53:52 +0800

    mongoose one user testok

--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,54 @@
 var express = require('express');
 var router = express.Router();
 var User = require("./User.js");
+var userlist = [];
 /* GET home page. */
 router.get('/', function(req, res, next) {  
-  console.log(req.session.user);
-  
-  if(req.session.user){
-    return res.render('chat',{user:req.session.user});
-  }
+  // req.session.user = req.session.user || null;
+  // console.log(req.session.user);  
+  // if(req.session.user){
+  //   return res.render('chat',{user:req.session.user});
+  // }
     res.render('index');
 });
 router.post("/registe",async function(req,res,next){
   
   const{name,password} = req.body;
+  // await User.remove();
   var user = new User({name:name,password:password});
   await user.save(function (err){
     if(err){
       console.log(err);
     }else{
       console.log("save ok");
-      
     }
   });
-  console.log(await User.find());
-  
+  console.log("注册用户"+await User.find());
   res.redirect('/');
 })
-router.post("/login",function(req,res){
+router.post("/login", async function(req,res){
   const {name, password } = req.body;
   req.session.user = {name,password};
-  if (user[name]&&name == user[name].name &&
-     password == user[name].password) {
+  // console.log(await User.findOne({name:"a"}));
+  const user = await User.findOne({name:name}); 
+  console.log("数据库用户"+user.name);
+  
+  // const query = User.find();
+  // const user =await  query.$where('this.name === name');
+  // console.log(user);
+  if (name == user.name &&
+     password == user.password) {
      console.log(req.session.user+"session的用户");
      
-     res.render('chat', { use:req.session.user })
+     res.render('chat', { use:req.session.user, })
   }
   res.redirect('/');
+});
+router.get("/login",function(req,res){
+  res.redirect("/");
+});
+router.get('/registe',function(req,res,next){
+  res.redirect('/');
 })
+
 module.exports = router;
