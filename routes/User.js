db31ac4e811b69abbf34e27b205ceee83a2434d3
haiming90 <weihaiming90@163.com>
2018-12-27 19:38:52 +0800

    version 1.1.1

--- routes/User.js
+++ routes/User.js
@@ -2,16 +2,9 @@ const mongoose = require("mongoose");
 mongoose.Promise = Promise;
 mongoose.connect("mongodb://localhost/test",{useMongoClient:true});
 const User = mongoose.model("User",(
-    {name:String,
+    {
+    name:String,
     password:String
-        }
+    }
 ));
-// async function run(){
-//     await User.remove();
-//     await User.create(
-//        {name:"hai",password:"hai"}
-//     )
-//     console.log(await User.find());    
-// }
-
  module.exports = User;
